fcea23ee8e120f687417375077904f9e1264b7e9
thinkmix <thinkmix@163.com>
2018-06-14 22:38:54 +0800

    tb;新增生成环境配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -31,13 +31,21 @@ module.exports = {
       path: '/home/nginx/html/ka_tas',
       'post-deploy' : 'yarn install && yarn build && pm2 reload ecosystem.config.js --env test'
     },    
+    onlineTest: {
+      user: 'root',
+      host: '47.98.43.180',
+      ref: 'origin/master',
+      repo: 'https://github.com/thinkmix/ka_tas.git',
+      path: '/usr/local/nginx/html/ka_tas',
+      'post-deploy' : 'yarn install && yarn build && pm2 reload ecosystem.config.js --env production'
+    }
     production: {
-        user: 'root',
-        host: '47.98.43.180',
-        ref: 'origin/master',
-        repo: 'https://github.com/thinkmix/ka_tas.git',
-        path: '/usr/local/nginx/html/ka_tas',
-        'post-deploy' : 'yarn install && yarn build && pm2 reload ecosystem.config.js --env production'
-      }
+      user: 'root',
+      host: '47.97.198.73',
+      ref: 'origin/master',
+      repo: 'https://github.com/thinkmix/ka_tas.git',
+      path: '/usr/local/nginx/html/ka_tas',
+      'post-deploy' : 'yarn install && yarn build && pm2 reload ecosystem.config.js --env production'
+    }
   }
 };
