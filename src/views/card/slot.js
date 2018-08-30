9f87f8ed9b4f1150a38bcdd7767d963e524b07c2
tengbo <thinkmix@163.com>
2018-08-31 06:01:09 +0800

    tb;调试结束

--- src/views/card/slot.js
+++ src/views/card/slot.js
@@ -47,16 +47,6 @@ var vm=new Moon({
 			});
 			Jsborya.webviewLoading({isLoad:false});//关闭app加载层
 
-			Jsborya.pageJump({
-				url:'',
-				stepCode:'801',
-				depiction:'登录',
-				data:{
-					phone:'17099999999'
-				}
-			});
-			return false;
-
 			Jsborya.getGuestInfo({
 				slot:'-2',
 				iccid:'',
