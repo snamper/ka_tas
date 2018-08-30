210e35938b3cc040318c2d4540953ed27e4d7ae0
tengbo <thinkmix@163.com>
2018-08-31 04:46:02 +0800

    2018083001

--- config/index.js
+++ config/index.js
@@ -61,7 +61,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../dist/2018083001/'),
     assetsSubDirectory: 'assets',
     assetsPublicPath: '/tasweb/2018083001/',
 
