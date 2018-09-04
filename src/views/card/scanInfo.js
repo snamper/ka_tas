97815578d1467d6a71d18dc3155e135ee90ccaf7
tengbo <thinkmix@163.com>
2018-09-04 18:25:48 +0800

    tb;BUG修复

--- src/views/card/scanInfo.js
+++ src/views/card/scanInfo.js
@@ -95,7 +95,6 @@ var vm=new Moon({
                 url:url,
                 stepCode:'999',
                 depiction:'开卡',
-                destroyed:false,
                 header:{
                     frontColor:'#ffffff',
                     backgroundColor:'#4b3887',
