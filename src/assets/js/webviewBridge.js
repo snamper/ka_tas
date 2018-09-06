bf270bd2e66949369735495e7e7e441f4633e2c9
tengbo <thinkmix@163.com>
2018-09-07 00:28:10 +0800

    tb;去除调试

--- src/assets/js/webviewBridge.js
+++ src/assets/js/webviewBridge.js
@@ -214,7 +214,7 @@ require('./base64.js');
 				name:'readCardICCID',
 				data:json,
 				callback:function(result){
-					alert('readCardICCID'+JSON.stringify(result));
+					//alert('readCardICCID'+JSON.stringify(result));
 					json.complete(result);
 				}
 			});
@@ -237,7 +237,7 @@ require('./base64.js');
 				name:'readCardIMSI',
 				data:json,
 				callback:function(result){
-					alert('readCardIMSI'+JSON.stringify(result));
+					//alert('readCardIMSI'+JSON.stringify(result));
 					json.complete(result);
 				}
 			});
@@ -248,12 +248,11 @@ require('./base64.js');
 			// 		'status':'1',
 			// 	});
 			// },1000)
-			alert('调用callWriteCard'+JSON.stringify(json));
 			callHandler({
 				name:'callWriteCard',
 				data:json,
 				callback:function(result){
-					alert('callWriteCard'+JSON.stringify(result));
+					//alert('callWriteCard'+JSON.stringify(result));
 					json.complete(result);
 				}
 			});
